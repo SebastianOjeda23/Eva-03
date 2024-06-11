0e92b75d9fd6f2f5b19e684572b45d401eaa2421
SebastianOjeda23 <169169884+SebastianOjeda23@users.noreply.github.com>
2024-06-11 11:38:29 -0400

    Inicio parte1
    Se agrega la estructura de la evaluacion + html con inputs

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+//const RecuperarDatos(()=>{
+    
+//})
+
+function RecuperarDatos()
+    let eNombre = document.getElementById("NombreCliente");
+    let eApellido = document.getElementById("ApellidoCliente");
+    let eRut = document.getElementById("RutCliente");
+    let eFecha = document.getElementById("FechaEntrega")
+    let eNombreJuego = document.getElementById("NombreJuego")
+    let eCompanny = document.getElementById("CompañiaJuego")
+    //Se dejo como Ecompanny ya que no soporta la Ñ
+    let eDireccion = document.getElementById("Direccion")
+    //console.log(RecuperarDatos) Se hace un console.log para ver por la consola si hay algun error
+    let eCredito = document.getElementById("Credito")
+    let eEfectivo = document.getElementById("Efectivo")
+    let vNombre = eNombre.value = vNombre
+    let vApellido = eApellido.value = vApellido
+
+
