8e30e5486cd59582baa61940ddc15e302f3fc6c5
SebastianOjeda23 <169169884+SebastianOjeda23@users.noreply.github.com>
2024-06-11 23:18:05 -0400

    eva 03 parte 2 ultimos avances
    se agrego el boton de actualizar y eliminar datos (aun queda un error y es el de que no tengo permiso para registrar datos)

--- assets/promesas.js
+++ assets/promesas.js
@@ -1,20 +1,28 @@
 import { db } from "./firebase.js";
 import {addDoc,collection,getDocs,doc,updateDoc,deleteDoc} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"
 export const registrarPersona = async(persona)=>{
-    const docRef = await addDoc(collection(db,"persona"),persona)
+    const docRef = await addDoc(collection(db, "persona"), persona)
 }
 ;
 
 export const obtenerPersona = async()=>{
-    //recupera la referencia (ruta)
-    const ref = collection(db,"persona");
-    //recuperar una captura de la base de datos
+    //Recupera la referencia (ruta)
+    const ref = collection(db, "persona");
+    //Recuperamos una captura de la bd
     const qSnap = await getDocs(ref);
-    let listado = []
-    qSnap.forEach((doc)=>{
+    let listado =  []
+    qSnap.forEach((doc) =>{
         console.log(doc.data());
         listado.push({...doc.data(),id:doc.id});
     });
     console.log(listado);
     return listado;
+}
+export const ActualizarPersona = async(objeto,id)=>{
+    const ref = doc(db,"persona",id)
+    await updateDoc(ref,objeto)
+}
+export const eliminarPersona = async(id)=>{
+    const ref = doc(db,"persona",id);
+    await deleteDoc(ref);
 }
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
+import { ActualizarPersona,eliminarPersona,obtenerPersona,registrarPersona } from "./promesas.js";
 
 window.addEventListener("load",()=>{
     document.getElementById("btnRegistro").addEventListener("click",registrar);
     cargarDatos();
+    document.getElementById("btnActualizar").addEventListener("click",actualizar);
 })
 
 const registrar = ()=>{
@@ -11,7 +13,7 @@ const registrar = ()=>{
     let eFechaEntrega = document.getElementById("FechaEntrega");
     let eNombreJuego = document.getElementById("NombreJuego");
     let eCompaniaJuego = document.getElementById("CompañiaJuego");
-    let eCantidadCopias = document.getElementById("CopiaJuegos");
+    let eCantidadCopias = document.getElementById("Copiajuegos");
     let eDireccion = document.getElementById("Direccion");
     let eCredito = document.getElementById("credito");
     let eEfectivo = document.getElementById("efectivo");
@@ -39,27 +41,119 @@ const registrar = ()=>{
         credito: vCredito,
         efectivo: vEfectivo
     };
-    console.log(objeto)
+    registrarPersona(objeto).then(()=>{
+        alert("Pedido realizado con excito!");
+        cargarDatos()
+    }).catch((error)=>{
+        console.log(error)
+    })
+    
 }   
 
-
-
-
-
-let colorOriginal = true;
-
-function cambiarContraste() {
-    const body = document.body;
-
-    if (colorOriginal) {
-        body.style.backgroundColor = "black";
-        body.style.color = "white";
-    } else {
-        body.style.backgroundColor = "white";
-        body.style.color = "black";
-    }
-
-    colorOriginal = !colorOriginal;
+const cargarDatos = ()=>{
+    //Traer todo lo registrado
+    obtenerPersona().then((persona)=>{
+        console.log(persona)
+        //cargarlo en la tabla del html
+        let estructura = ""
+        persona.forEach((p)=>{
+            estructura += "<tr>"
+            estructura += "<td>"+p.nombre+"<td>"
+            estructura += "<td>"+p.apellido+"<td>"
+            estructura += "<td>"+p.rut+"<td>"
+            estructura += "<td>"+p.FechaEntrega+"<td>"
+            estructura += "<td>"+p.nombrejuego+"<td>"
+            estructura += "<td>"+p.companiajuego+"<td>"
+            estructura += "<td>"+p.copias+"<td>"
+            estructura += "<td>"+p.credito+"<td>"
+            estructura += "<td>"+p.efectivo+"<td>"
+            estructura += "<td><button id='UPD"+p.id+"'>Actualizar</button></td>"
+            estructura += "<td><button id='DEL"+p.id+"'>Eliminar</button></td>"
+            estructura += "</tr>"
+        })
+        document.getElementById("CuerpoTabla").innerHTML = estructura;
+        
+        persona.forEach((p)=>{
+            let elemento = document.getElementById("UPD"+p.id);
+            elemento.addEventListener("click",()=>{
+                document.getElementById("UPDnombre").value =p.nombre;
+                document.getElementById("UPDApellidoCliente").value =p.apellido;
+                document.getElementById("UPDRutCliente").value =p.rut;
+                document.getElementById("UPDFechaEntrega").value =p.FechaEntrega;
+                document.getElementById("UPDNombreJuego").value =p.nombrejuego;
+                document.getElementById("UPDCompañiaJuego").value =p.companiajuego;
+                document.getElementById("UPDCopiajuegos").value =p.copias;
+                document.getElementById("UPDDireccion").value =p.direccion;
+                document.getElementById("UPDcredito").value =p.credito;
+                document.getElementById("UPDefectivo").value =p.efectivo;
+                document.getElementById("btnactualizar").value =p.id;
+            });
+            let btnEliminar = document.getElementById("DEL"+p.id);
+            btnEliminar.addEventListener("click",()=>{
+                if(confirm("Desea eliminar el pedido de: \n"+p.nombre+" "+p.apellido)){
+                    console,log("vamos a eliminar")
+                    eliminarPersona(p.id).then(()=>{
+                        alert("eliminaste con exito!")
+                        cargarDatos();
+                    }).catch((e)=>{
+                        console,log(e)
+                    })
+                    
+                }else(
+                    console.log("cancelaste la eliminacion")
+                )
+            })
+        })
+    })
 }
-//Esta funcion nos permite cambiar el color de la pagina y el contraste de esta
+
+const actualizar = ()=>{
+    let eNombre = document.getElementById("UPDNombreCliente");
+    let eApellido = document.getElementById("UPDApellidoCliente");
+    let eRut = document.getElementById("UPDRutCliente");
+    let eFechaEntrega = document.getElementById("UPDFechaEntrega");
+    let eNombreJuego = document.getElementById("UPDNombreJuego");
+    let eCompaniaJuego = document.getElementById("UPDCompañiaJuego");
+    let eCantidadCopias = document.getElementById("UPDCopiajuegos");
+    let eDireccion = document.getElementById("UPDDireccion");
+    let eCredito = document.getElementById("UPDcredito");
+    let eEfectivo = document.getElementById("UPDefectivo");
+
+    let vNombre = eNombre.value;
+    let vApellido = eApellido.value;
+    let vRut = eRut.value;
+    let vFechaEntrega = eFechaEntrega.value;
+    let vNombreJuego = eNombreJuego.value;
+    let vCompaniaJuego = eCompaniaJuego.value;
+    let vCantidadCopias = eCantidadCopias.value;
+    let vDireccion = eDireccion.value;
+    let vCredito = eCredito.checked; 
+    let vEfectivo = eEfectivo.checked;
+
+    let objeto = {
+        nombre: vNombre,
+        apellido: vApellido,
+        rut: vRut,
+        FechaEntrega: vFechaEntrega,
+        nombrejuego: vNombreJuego,
+        companiajuego: vCompaniaJuego,
+        copias: vCantidadCopias,
+        direccion: vDireccion,
+        credito: vCredito,
+        efectivo: vEfectivo};
+let id = document.getElementById("btnactualizar").value
+//envia el objeto y el id a las promesas
+
+//cargar algo tipo loading
+document.getElementById("btnactualizar").disabled = "True";
+ActualizarPersona(objeto,id).then(()=>{
+    alert("Se actualizo con excito")
+    cargarDatos();
+    document.getElementById("btnactualizar").disabled = "";
+}).catch((e)=>{
+    console.log(e)
+}).finally(()=>{
+    document.getElementById("btnactualizar").disabled = ""
+})
+}
 
